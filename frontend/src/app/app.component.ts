d6d6b04c14a320462b0912a5326f52bf0e4f7c5d
Akshar Rawal <akshar.1006@gmail.com>
2022-12-03 11:35:57 -0500

    sidebar stuff

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -14,5 +14,52 @@ export class requestTokensDTO {
     styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-    wallet: string | undefined;
+    wallet: ethers.Wallet | undefined;
+
+    provider: ethers.providers.Provider;
+
+    backendUrl: string | undefined;
+
+    ethBalance: number | string | undefined;
+
+    errorMsg: string | undefined;
+
+    constructor() {
+        // set the provider object
+        this.provider = ethers.getDefaultProvider('goerli');
+
+        this.backendUrl = "http://localhost:3000"
+    }
+
+
+    createWallet() {
+        this.wallet = ethers.Wallet.createRandom().connect(this.provider);
+    }
+
+
+    importWallet(secret: string, importMethod: string) {
+        if (importMethod == 'mnemonic') {
+            this.wallet = ethers.Wallet.fromMnemonic(secret ?? "").connect(this.provider);
+        } else {
+            this.wallet = new ethers.Wallet(secret ?? "").connect(this.provider);
+        }
+        if (this.wallet.address.length == 42) {
+            this.updateValues();
+        } else {
+            this.errorMsg = 'Could not import wallet, invalid mnumonic or private key';
+            console.log(this.errorMsg);
+            alert(this.errorMsg);
+        }
+    }
+
+
+    async updateValues() {
+        [this.ethBalance] = [
+            'loading...'
+        ];
+
+        this.wallet?.getBalance().then((balance) => {
+            this.ethBalance = parseFloat(ethers.utils.formatEther(balance));
+        });
+    }
 }
