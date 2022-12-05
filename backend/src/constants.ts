747fa802a86147591b23b6bf66e62b0ca9c6c71d
Akshar Rawal <akshar.1006@gmail.com>
2022-12-05 11:26:23 -0500

    backend to serve contract addresses

--- backend/src/constants.ts
+++ backend/src/constants.ts
@@ -1,2 +1,2 @@
-export const ballotContractAddress: string = "";
-export const tokenContractAddress: string = "0x284A7042be8749c1b3A35509F27ebb09c2737956";
+export const lotteryContractAddress: string = "0xA520E49A056D9eB838d9c5D316c636B03B11Dad9";
+export const tokenContractAddress: string = "0x37C33e20d4766F84e1a18053519Aa2BA66bF0D4a";
